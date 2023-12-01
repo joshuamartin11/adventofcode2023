636d833e89ac3f0879e8dba8e3f2e605139d3eb2
Joshua Martin <77560324+joshuamartin11@users.noreply.github.com>
2023-12-01 16:07:38 +1000

    Added comments

--- D1/D1P1.py
+++ D1/D1P1.py
@@ -1,13 +1,16 @@
 with open('D1\input.txt') as f:
     lines = f.readlines()
     val = 0
+    #Read through the lines
     for line in lines:
+        #read through each character
         for c in line:
-            if c.isnumeric():
+            #
+            if c.isdigit():
                 v1 = c
                 break
         for c in reversed(line):
-            if c.isnumeric():
+            if c.isdigit():
                 v2 = c
                 break
         val += int(v1 + v2)
